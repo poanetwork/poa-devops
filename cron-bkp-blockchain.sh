f0cda0902d483db9f96ee96bd6e1a27edbe13a96
phahulin <p.hahulin@hintsolutions.ru>
2018-01-03 14:29:48 +0300

    Print date to crons log files

--- cron-bkp-blockchain.sh
+++ cron-bkp-blockchain.sh
@@ -9,4 +9,12 @@ set -u
 set -o
 
 cd /root/poa-devops
+
+echo "$(date -u)" > out.txt
+echo "$(date -u)" > err.txt
+
+# actual command
 /usr/local/bin/ansible-playbook -i hosts -c local site.yml > out.txt 2> err.txt
+
+echo "" > out.txt
+echo "" > err.txt
